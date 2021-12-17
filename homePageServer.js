2f858ac5447e065ddbf949d7350fb9979197c918
szymon <szymon.wall.123456789@gmail.com>
2021-12-17 21:54:50 +0100

    21:54:50,08 17.12.2021

--- homePageServer.js
+++ homePageServer.js
@@ -37,7 +37,7 @@ server.get('/sundayFunday', (req, res) => {
 })
 
 server.post('/', (req, res) => {
-  res.status(201).send('kap')
+  res.status(201).send('kapp')
 })
 
 server.listen(process.env.PORT || 2137)
