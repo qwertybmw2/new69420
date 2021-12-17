fd3494f6fc91781d9f904dfa62d701ec076bfe40
szymon <szymon.wall.123456789@gmail.com>
2021-12-17 23:23:37 +0100

    23:23:37,42 17.12.2021

--- homePageServer.js
+++ homePageServer.js
@@ -6,7 +6,7 @@ var express = require('express')
 var mongoose = require('mongoose')
 
 var urlencodedParser = bodyParser.urlencoded({extended: false})
-var app = express()\
+var app = express()
 var dbURI = 'mongodb+srv://qwertybmw:mongodbpassword@cluster0.uehtx.mongodb.net/POGGIES?retryWrites=true&w=majority'
 mongoose.connect(dbURI, {useNewUrlParser: true, useUnifiedTopology: true})
 .then((result) => console.log('BRUH'))
