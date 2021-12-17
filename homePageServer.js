e9cf335e62787233ee0433bb5f4edc76563fd231
szymon <szymon.wall.123456789@gmail.com>
2021-12-17 23:42:01 +0100

    23:42:01,68 17.12.2021

--- homePageServer.js
+++ homePageServer.js
@@ -1,23 +1,49 @@
 // VARIABLES
 {
-var bodyParser = require('body-parser')
-var fs = require('fs')
-var express = require('express')
-var mongoose = require('mongoose')
+const bodyParser = require('body-parser')
+const fs = require('fs')
+const express = require('express')
+const mongoose = require('mongoose')
 
-var urlencodedParser = bodyParser.urlencoded({extended: false})
-var app = express()
-var dbURI = 'mongodb+srv://qwertybmw:mongodbpassword@cluster0.uehtx.mongodb.net/POGGIES?retryWrites=true&w=majority'
+const urlencodedParser = bodyParser.urlencoded({extended: false})
+const app = express()
+const Schema = mongoose.Schema
+const dbURI = 'mongodb+srv://qwertybmw:mongodbpassword@cluster0.uehtx.mongodb.net/POGGIES?retryWrites=true&w=majority'
 mongoose.connect(dbURI, {useNewUrlParser: true, useUnifiedTopology: true})
 .then((result) => app.listen(process.env.PORT || 2137))
 }
 
 // Pepega Megaphone JSOOOOOOOOON
 {
- searchHistoryBuffer = fs.readFileSync('public/homePage/searchHistory.json')
- searchHistoryParsed = JSON.parse(searchHistoryBuffer)
-//
-// fs.writeFileSync('public/homePage/searchHistory.json', JSON.stringify(searchHistoryParsed, null, 2))
+  const blogSchema = new Schema({
+    title: {
+      type: String,
+      required: true
+    },
+    snippet: {
+      type: String,
+      required: true
+    },
+    body: {
+      type: String,
+      required: true
+    }
+  }, {timestamps: true})
+
+  const Blog = mongoose.model('Blog', blogSchema)
+}
+{
+  app.get('/add-blog', (req, res) => {
+    const blog = new Blog({
+      title: 'new blog',
+      snippet: 'about my new blog',
+      body: 'more about my new blog'
+    })
+    blog.save()
+    .then((result) => {
+      res.send(result)
+    })
+  })
 }
 
 // Server
