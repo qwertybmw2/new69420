7fac2607d2cce18849a9f4737f7d5fb956e0f03e
szymon <szymon.wall.123456789@gmail.com>
2021-12-18 15:10:36 +0100

    15:10:36,00 18.12.2021

--- homePageServer.js
+++ homePageServer.js
@@ -8,8 +8,8 @@ const mongoose = require('mongoose')
 const urlencodedParser = bodyParser.urlencoded({extended: false})
 const app = express()
 const Schema = mongoose.Schema
-const dbURI = 'mongodb+srv://qwertybmw:mongodbpassword@cluster0.uehtx.mongodb.net/POGGIES?retryWrites=true&w=majority'
-mongoose.connect(dbURI, {useNewUrlParser: true, useUnifiedTopology: true}).then((result) => app.listen(process.env.PORT || 2137))
+//const dbURI = 'mongodb+srv://qwertybmw:mongodbpassword@cluster0.uehtx.mongodb.net/POGGIES?retryWrites=true&w=majority'
+//mongoose.connect(dbURI, {useNewUrlParser: true, useUnifiedTopology: true}).then((result) => app.listen(process.env.PORT || 2137))
 }
 
 // Pepega Megaphone JSOOOOOOOOON
@@ -53,4 +53,6 @@ app.get('/sundayFunday', (req, res) => {
 app.post('/', (req, res) => {
   res.send('kappa')
 })
+
+app.listen(process.env.PORT)
 }
