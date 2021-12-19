49b8b9fa13358f3f6982dd7d4c4b39bc23ec0c33
szymon <szymon.wall.123456789@gmail.com>
2021-12-19 21:11:25 +0100

    21:11:25,68 19.12.2021

--- public/solidGame/solidGameMenu.js
+++ public/solidGame/solidGameMenu.js
@@ -27,7 +27,7 @@ loginButton.addEventListener('click', (e) => {
     },
     body: JSON.stringify({username: loginInput.value, password: passwordInput.value})// these are credentials
   }).then((result) => {
-    location.href = response.url
+    location.href = result.url
   })
 })
 }
