94ca8b40ed021beb90de5a2fbf84d83c6784cc4f
szymon <szymon.wall.123456789@gmail.com>
2021-12-23 21:36:30 +0100

    21:36:30,48 23.12.2021

--- public/sundayFunday/sundayFunday.js
+++ public/sundayFunday/sundayFunday.js
@@ -41,7 +41,7 @@ for (var i = 0; i < world0.length; i++) {
         },
         body: JSON.stringify({
           x: parseFloat(world[0].style.left) + zoom * .125,
-          y: parseFloat(world[0].style.top) + zoom * .125
+          y: parseFloat(world[0].style.top)
         })
       })
     }, 10000
@@ -70,63 +70,63 @@ for (var i = 0; i < world0.length; i++) {
       zoomer()
       zoomControl()
     })
+    addEventListener('keydown', (e) => {
+      if (e.key === 'w') {
+        wPressed = true
+      }
+      if (e.key === 'a') {
+        aPressed = true
+      }
+      if (e.key === 's') {
+        sPressed = true
+      }
+      if (e.key === 'd') {
+        dPressed = true
+      }
+    })
+    addEventListener('keyup', (e) => {
+      if (e.key === 'w') {
+        wPressed = false
+        playerPixel[59].style.backgroundColor = '#520'
+        playerPixel[60].style.backgroundColor = '#520'
+        playerPixel[45].style.backgroundColor = '#e96'
+        playerPixel[42].style.backgroundColor = '#e96'
+      }
+      if (e.key === 'a') {
+        aPressed = false
+        playerPixel[26].style.backgroundColor = '#000'
+        playerPixel[29].style.backgroundColor = '#000'
+        playerPixel[34].style.backgroundColor = '#e96'
+        playerPixel[36].style.backgroundColor = '#000'
+        playerPixel[37].style.backgroundColor = '#e96'
+        playerPixel[42].style.backgroundColor = '#e96'
+        playerPixel[44].style.backgroundColor = '#226'
+        playerPixel[45].style.backgroundColor = '#e96'
+      }
+      if (e.key === 's') {
+        sPressed = false
+        playerPixel[59].style.backgroundColor = '#520'
+        playerPixel[60].style.backgroundColor = '#520'
+        playerPixel[45].style.backgroundColor = '#e96'
+        playerPixel[42].style.backgroundColor = '#e96'
+      }
+      if (e.key === 'd') {
+        dPressed = false
+        playerPixel[26].style.backgroundColor = '#000'
+        playerPixel[29].style.backgroundColor = '#000'
+        playerPixel[34].style.backgroundColor = '#e96'
+        playerPixel[35].style.backgroundColor = '#000'
+        playerPixel[37].style.backgroundColor = '#e96'
+        playerPixel[42].style.backgroundColor = '#e96'
+        playerPixel[43].style.backgroundColor = '#226'
+        playerPixel[45].style.backgroundColor = '#e96'
+      }
+    })
   })
 }
 
 // event listeners
 {
-addEventListener('keydown', (e) => {
-  if (e.key === 'w') {
-    wPressed = true
-  }
-  if (e.key === 'a') {
-    aPressed = true
-  }
-  if (e.key === 's') {
-    sPressed = true
-  }
-  if (e.key === 'd') {
-    dPressed = true
-  }
-})
-addEventListener('keyup', (e) => {
-  if (e.key === 'w') {
-    wPressed = false
-    playerPixel[59].style.backgroundColor = '#520'
-    playerPixel[60].style.backgroundColor = '#520'
-    playerPixel[45].style.backgroundColor = '#e96'
-    playerPixel[42].style.backgroundColor = '#e96'
-  }
-  if (e.key === 'a') {
-    aPressed = false
-    playerPixel[26].style.backgroundColor = '#000'
-    playerPixel[29].style.backgroundColor = '#000'
-    playerPixel[34].style.backgroundColor = '#e96'
-    playerPixel[36].style.backgroundColor = '#000'
-    playerPixel[37].style.backgroundColor = '#e96'
-    playerPixel[42].style.backgroundColor = '#e96'
-    playerPixel[44].style.backgroundColor = '#226'
-    playerPixel[45].style.backgroundColor = '#e96'
-  }
-  if (e.key === 's') {
-    sPressed = false
-    playerPixel[59].style.backgroundColor = '#520'
-    playerPixel[60].style.backgroundColor = '#520'
-    playerPixel[45].style.backgroundColor = '#e96'
-    playerPixel[42].style.backgroundColor = '#e96'
-  }
-  if (e.key === 'd') {
-    dPressed = false
-    playerPixel[26].style.backgroundColor = '#000'
-    playerPixel[29].style.backgroundColor = '#000'
-    playerPixel[34].style.backgroundColor = '#e96'
-    playerPixel[35].style.backgroundColor = '#000'
-    playerPixel[37].style.backgroundColor = '#e96'
-    playerPixel[42].style.backgroundColor = '#e96'
-    playerPixel[43].style.backgroundColor = '#226'
-    playerPixel[45].style.backgroundColor = '#e96'
-  }
-})
 }
 
 // functions
