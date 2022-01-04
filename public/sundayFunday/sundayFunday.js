e9afe21907a55bfb646ec3e7f58a575be4742a3b
szymon <szymon.wall.123456789@gmail.com>
2022-01-04 10:40:50 +0100

    10:40:50,29 04.01.2022

--- public/sundayFunday/sundayFunday.js
+++ public/sundayFunday/sundayFunday.js
@@ -121,9 +121,9 @@ for (var i = 0; i < world.length; i++) {
 {
   function movement() {
   if (wPressed) {
-    for (var i = 0; i < world.length; i++) {
+  //  for (var i = 0; i < world.length; i++) {
       document.getElementById('world' + i).style.top = parseFloat(document.getElementById('world' + i).style.top) + zoom * .125 + 'vw'
-    }
+  //  }
   }
   if (aPressed) {
     for (var i = 0; i < world.length; i++) {
