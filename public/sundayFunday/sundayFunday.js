be0c2d2060c2c13b6c093ad18fa17681511ff766
szymon <szymon.wall.123456789@gmail.com>
2022-01-04 10:36:02 +0100

    10:36:02,06 04.01.2022

--- public/sundayFunday/sundayFunday.js
+++ public/sundayFunday/sundayFunday.js
@@ -43,10 +43,10 @@ for (var i = 0; i < world.length; i++) {
     return result.json()
   }).then((json) => {
     for (var i = 0; i < world.length; i++) {
-      document.getElementById('world' + i).style.top = coordinates.y + 'vw'
+      document.getElementById('world' + i).style.top = json.y + 'vw'
     }
     for (var i = 0; i < world.length; i++) {
-      document.getElementById('world' + i).style.left = coordinates.x + 'vw'
+      document.getElementById('world' + i).style.left = json.x + 'vw'
     }
     addEventListener('wheel', (e) => {
       switch (Math.sign(e.deltaY)) {
