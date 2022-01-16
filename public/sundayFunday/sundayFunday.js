444f22476038175f8a5528e7bf6512348e3bf4aa
szymon <szymon.wall.123456789@gmail.com>
2022-01-16 18:08:00 +0100

    18:08:00,38 16.01.2022

--- public/sundayFunday/sundayFunday.js
+++ public/sundayFunday/sundayFunday.js
@@ -244,6 +244,9 @@ var timer = 0
       x: Math.floor((50 - parseFloat(document.getElementById('world0').style.left)) / zoom / 25),
       y: Math.floor((-50 * 0.5625 + parseFloat(document.getElementById('world0').style.top)) / zoom / 25) + 1
     }
+    for (var i = 0; i < 64; i++) {
+      console.log(i)
+    }
     console.log(playerCoordinates)
   }
 }
