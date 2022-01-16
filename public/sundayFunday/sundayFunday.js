e32e526140f7091effaa6b75a8055d860bfef6d5
szymon <szymon.wall.123456789@gmail.com>
2022-01-16 18:50:36 +0100

    18:50:36,64 16.01.2022

--- public/sundayFunday/sundayFunday.js
+++ public/sundayFunday/sundayFunday.js
@@ -250,7 +250,7 @@ var timer = 0
         for (var i = 0; i < world.length; i++) {
           if (worldPositions[i].x === playerCoordinates + x &&
               worldPositions[i].y === playerCoordinates + y) {
-            newWorld = false
+            createWorld = false
           }
         }
         if (createWorld) {
