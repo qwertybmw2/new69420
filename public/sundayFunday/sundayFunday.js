0c7aee5f59d0d94ea0340aa60de501365e724ef7
szymon <szymon.wall.123456789@gmail.com>
2022-01-26 17:21:16 +0100

    17:21:16,28 26.01.2022

--- public/sundayFunday/sundayFunday.js
+++ public/sundayFunday/sundayFunday.js
@@ -246,13 +246,19 @@ var timer = 0
       x: Math.floor((50 - parseFloat(document.getElementById('world0').style.left)) / zoom / 25),
       y: Math.floor((-50 * 0.5625 + parseFloat(document.getElementById('world0').style.top)) / zoom / 25) + 1
     }
-    for (var x = -1; x < 2; x++) {
-      for (var y = -1; y < 2; y++) {
+    for (var x = -4; x < 5; x++) {
+      for (var y = -4; y < 5; y++) {
         createWorld = true
         for (var i = 0; i < worldPositions.length; i++) {
           if (worldPositions[i].x === playerCoordinates.x + x &&
               worldPositions[i].y === playerCoordinates.y + y) {
             createWorld = false
+          }
+          if (worldPositions[i].x < playerCoordinates.x - 4 ||
+              worldPositions[i].x > playerCoordinates.y + 4 ||
+              worldPositions[i].y < playerCoordinates.y - 4 ||
+              worldPositions[i].y > playerCoordinates.y + 4) {
+            document.getElementById('world' + i).remove()
           }
         }
         if (createWorld) {
