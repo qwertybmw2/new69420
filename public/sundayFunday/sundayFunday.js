02303e6ec957af31435e118a9878fcd253265850
szymon <szymon.wall.123456789@gmail.com>
2022-02-04 21:33:06 +0100

    21:33:05,41 04.02.2022

--- public/sundayFunday/sundayFunday.js
+++ public/sundayFunday/sundayFunday.js
@@ -272,7 +272,7 @@ settings.addEventListener('click', (e) => {
       y: Math.floor((-50 * 0.5625 + parseFloat(document.getElementById('world0').style.top)) / zoom / 25) + 1
     }
     for (var x = -3; x < 4; x++) {
-      for (var y = -2; y < 3; y++) {
+      for (var y = -1; y < 2; y++) {
         createWorld = true
         for (var i = 0; i < worldPositions.length; i++) {
           if (worldPositions[i].x === playerCoordinates.x + x &&
