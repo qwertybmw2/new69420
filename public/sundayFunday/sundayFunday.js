296d6d2b34387e72c3397633a821a15b351fe0f4
szymon <szymon.wall.123456789@gmail.com>
2022-02-05 17:30:36 +0100

    17:30:35,68 05.02.2022

--- public/sundayFunday/sundayFunday.js
+++ public/sundayFunday/sundayFunday.js
@@ -17,7 +17,7 @@ var timer = 0
     worldi.classList = 'world'
     worldi.id = 'world' + i
     document.body.prepend(worldi)
-    for (var j = 0; j < 400; j++) {
+    for (var j = 0; j < 100; j++) {
       worldi.appendChild(document.createElement('div'))
     }
   }
@@ -254,7 +254,7 @@ settings.addEventListener('click', (e) => {
         worldi.classList = 'world'
         worldi.id = 'world' + i
         document.body.prepend(worldi)
-        for (var j = 0; j < 400; j++) {
+        for (var j = 0; j < 100; j++) {
           document.getElementById('world' + i).appendChild(document.createElement('div'))
         }
         generatingWorld = false
@@ -266,7 +266,7 @@ settings.addEventListener('click', (e) => {
         worldi.classList = 'world'
         worldi.id = 'world' + i
         document.body.prepend(worldi)
-        for (var j = 0; j < 400; j++) {
+        for (var j = 0; j < 100; j++) {
           worldi.appendChild(document.createElement('div'))
         }
       }
